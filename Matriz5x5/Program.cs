e8b69f3262c04aceec65b80e29495210cb8059f7
Kichinda34 <leandroschneiderlima@gmail.com>
2023-03-09 11:51:11 -0300

    Arrumada a impressão no código e a soma da Diagonal Secundária incrementada.

--- Matriz5x5/Program.cs
+++ Matriz5x5/Program.cs
@@ -2,46 +2,72 @@
 {
     private static void Main(string[] args)
     {
-        int[,] matriz = new int [5, 5];
-        int media = 0;
-        int somaLinha = 0;
-        int somaColuna = 0;
+        int[,] matriz = new int[5, 5];
+        int somaDiagonal = 0;
+        int somaInversa = 0;
+        Random n = new Random();
 
-        for (int i = 0; i < 5; i++)
+        //Somar valores na matriz
+        for (int c = 0; c < matriz.GetLength(1); c++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int l = 0; l < matriz.GetLength(0); l++)
             {
-                Random n = new Random();
-                int aux = n.Next(100);
-                matriz[i, j] = aux;
+                matriz[l, c] = n.Next(1000);
             }
         }
 
         //somas de linhas
-        for (int c = 0; c < 5; c++)
+        Console.WriteLine("Soma das colunas...");
+        for (int l = 0; l < matriz.GetLength(1); l++)
         {
-            somaLinha = somaLinha + matriz[0, c];
+            Console.WriteLine("A soma das linha [" + l + "]= ");
+            int somaLinha = 0;
+            for (int c = 0; c < matriz.GetLength(1); c++)
+            {
+                somaLinha += matriz[l, c];
+            }
             Console.WriteLine(somaLinha);
         }
-        //soma coluna
-        for (int l = 0; l < 5; l++)
+
+        //soma de colunas
+        Console.WriteLine("Soma das colunas...");
+        for (int c = 0; c < matriz.GetLength(1); c++)
         {
-            somaColuna = somaColuna + matriz[l, 0];
+            Console.WriteLine("A soma das Colunas [" + c + "]= ");
+            int somaColuna = 0;
+            for (int l = 0; l < matriz.GetLength(0); l++)
+            {
+                somaColuna += matriz[l, c];
+            }
             Console.WriteLine(somaColuna);
         }
 
+        //soma de diagonais
+        Console.WriteLine("Soma da Diagonal Principal...");
 
-        //soma diagonal
-        for (int l = 0;l < 5; l++)
+        for (int c = 0; c < matriz.GetLength(1); c++)
         {
-            for(int c = 0;c < 5; c++)
+            somaDiagonal += matriz[c, c];
+        }
+        Console.WriteLine(somaDiagonal);
+
+        //Soma da Diagonal Secundaria
+        Console.WriteLine("Soma da Diagonal Secundária...");
+        for (int c = 0; c < matriz.GetLength(1); c++)
+        {
+            somaInversa += matriz[c, matriz.GetLength(1) - c - 1];
+        }
+        Console.WriteLine(somaInversa);
+
+        //impressão da Matriz
+        Console.WriteLine();
+        for (int l = 0; l < matriz.GetLength(0); l++)
+        {
+            for (int c = 0; c < matriz.GetLength(1) - 1; c++)
             {
-                if (l == c)
-                {
-                    media = matriz[l, c] + matriz[l, c];
-                }
+                Console.Write("\tMat[" + l + "][" + c + "] "+ matriz[l, c].ToString("d4"));
             }
-            Console.WriteLine(media);
+            Console.WriteLine();
         }
     }
 }
